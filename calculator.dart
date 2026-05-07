cba18564ee60d03ba97f45e7220d4f7e2e61a341
myarenaktas <130570723+myarenaktas@users.noreply.github.com>
2026-05-07 08:41:59 +0000

    add calculator

---
+++ calculator.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+void main() {
+  print("Scientific Calculator");
+
+  double a = 25;
+  double b = 5;
+
+  print("Addition: ${a + b}");
+  print("Subtraction: ${a - b}");
+  print("Multiplication: ${a * b}");
+  print("Division: ${a / b}");
+  print("Power: ${pow(a, 2)}");
+  print("Square Root: ${sqrt(a)}");
+  print("Sin: ${sin(a)}");
+  print("Cos: ${cos(a)}");
+  print("Tan: ${tan(a)}");
+  print("Log: ${log(a)}");
+}
